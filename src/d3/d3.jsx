7e39cba6eba80c163fceef44dae77e633b162724
HyunJoo Kim <hyunjoogo@gmail.com>
2021-05-17 17:47:04 +0900

    D3 + React Basics

--- src/d3/d3.jsx
+++ src/d3/d3.jsx
@@ -1,37 +1,35 @@
-import React, { useRef, useEffect, useState } from "react";
-import { select } from "d3";
+import React, {useRef, useEffect, useState} from "react";
+import styles from './d3.module.css';
+import {select} from "d3";
 
-export default(props) => {
-  const [data, setData] = useState([24, 30, 45, 70, 26]);
-  const svgRef = useRef(null);
+
+const D3 = (props) => {
+  const [data, setData] = useState([25, 30, 45, 60, 20]);
+  const svgRef = useRef();
 
   useEffect(() => {
-    const svg = select(svgRef.current); // selection 객체
+    const svg = select(svgRef.current);
     svg
       .selectAll("circle")
       .data(data)
-      .join(
-        (enter) => enter.append("circle"),
-        (update) => update.attr("class", "updated"),
-        (exit) => exit.remove()
-      )
-      .attr("r", (value) => value)
-      .attr("cx", (value) => value * 2)
-      .attr("cy", (value) => value * 2)
+      .join("circle")
+      .attr("r", value => value)
+      .attr("cx", value => value * 2)
+      .attr("cy", value => value * 2)
       .attr("stroke", "red");
   }, [data]);
-
   return (
-    <>
-      <svg ref={svgRef}>
-        {data.map((el, i) => (
-          <circle r={el} key={i}>
-            {el}
-          </circle>
-        ))}
-      </svg>
-      <button onClick={() => {setData(data.map(el => el + 5))}}>increase + 5</button>
-      <button onClick={() => {setData(data.filter(el => el > 35))}}>filter circle r should gt 35</button>
-    </>
-  );
-}
+    <React.Fragment>
+      <svg ref={svgRef}></svg>
+      <br/>
+      <button onClick={() => setData(data.map(value => value + 5))}>
+        Update data
+      </button>
+      <button onClick={() => setData(data.filter(value => value < 35))}>
+        Filter data
+      </button>
+    </React.Fragment>
+  )
+};
+
+export default D3;
